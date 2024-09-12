676bfa6f297a190201cfa64e0a030dc94e5a2cfe
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-09-11 22:15:29 -0300

    add tipoToken

---
+++ src/main/java/br/uepg/deinfo/prgA24/Tokens/TokenTipo.java
@@ -0,0 +1,34 @@
+package br.uepg.deinfo.prgA24.Tokens;
+
+public enum TokenTipo {
+    //Palavras reservadas
+    PROGRAM,
+    INT,
+    THEN,
+    END,
+    IF,
+    ENDPROGRAM,
+    FUNCTION,
+    ENDFUNCTION, 
+    DECLARE,
+    ENDDECLARE,
+    RETURN,
+    PRINT,
+    CALL,
+
+    //simbolos
+    PONTOVIRGULA, 
+    VIRGULA,
+    ABREPARENTESES,
+    FECHAPARENTESES,
+
+    //operadores
+    SOMA,
+    SUBTRACAO,
+    MULTIPLICACAO,
+    DIVISAO,
+    IGUAL,
+    MENOR,
+    MAIOR,
+    ATRIBUICAO
+}
