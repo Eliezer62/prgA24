2bd75c56661fb5fff78782e392b0e357c6766f58
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-09-19 17:10:37 -0300

    app para suportar arquivos externos

--- src/main/java/br/uepg/deinfo/prgA24/App.java
+++ src/main/java/br/uepg/deinfo/prgA24/App.java
@@ -1,10 +1,29 @@
 package br.uepg.deinfo.prgA24;
 
-/**
- * Hello world!
- */
+import java.io.File;
+
+import br.uepg.deinfo.prgA24.Exception.TokenInvalido;
+
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        if(args.length != 2)
+        {
+            System.err.println("Uso -f arquivo.prg");
+            System.exit(-1);
+        }
+        File arquivo = new File(args[1]);
+        Scanner sc = new Scanner(arquivo);
+        
+        try {
+            Parse analisador = new Parse(sc.getTabela());
+            analisador.analisar();
+        } catch (TokenInvalido e) {
+            System.err.println("token invalido "+ e.getMessage());
+        }
+        catch(RuntimeException e)
+        {
+            System.err.println(e.getMessage());
+        }
     }
 }
